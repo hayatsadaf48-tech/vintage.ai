be9129e5d6eb0c9b9d13d0351f2b8afc071d6bee
kashif <kashif.iitp@gamail.com>
2026-06-01 19:59:32 +0530

    Fix SMTP

--- Ai-interview-1-main/server.js
+++ Ai-interview-1-main/server.js
@@ -27,13 +27,15 @@ const app = express();
 
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
